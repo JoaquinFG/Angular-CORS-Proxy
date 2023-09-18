b53a7cc0884e27e6b5679b14f4ae774083704517
joaquin.ferreira <133866030+JoaquinFerreiraBabel@users.noreply.github.com>
2023-09-18 16:41:27 +0200

    add error Cors function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-CORS';
+  constructor(private http: HttpClient) {}
+
+  hacerPeticion() {
+    this.http.get('https://example.com')
+      .pipe(
+        catchError(error => {
+          console.log('Error al hacer la petición:', error.message);
+          if (error instanceof HttpErrorResponse && error.status === 0) {
+            console.log('Posiblemente sea un error CORS.');
+          }
+          return of(null);
+        })
+      )
+      .subscribe(data => {
+        if (data) {
+          console.log(data);
+        }
+      });
+  }
+  
+  
 }
